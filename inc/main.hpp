c1506926eed411960a1663d7ac68b85fcc6fd714
comradecobweb <comradecobweb@gmail.com>
2024-10-22 19:23:42 +0200

    Added support for program arguments

--- inc/main.hpp
+++ inc/main.hpp
@@ -12,6 +12,19 @@
 
 int main(int argc, char **argv);
 static void menu();
+static void help();
+
+///
+/// @brief Checks if the current element is the last element of an array of elements.
+/// @param index Current element.
+/// @param argc Array size (count of elements).
+/// @return True if yes, false if no.
+inline bool isLast(const int &index, const int & argc)
+{
+    return index == argc - 1;
+}
+
+int getNextValue(int &index, const int &argc, char *argv[]);
 
 
 #endif //MAIN_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,94 @@
 #include "main.hpp"
 
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
     using namespace std;
     if(argc > 1)
     {
-        // to do in future
-        menu();
+        string s(argv[1]);
+
+        if (s=="help" || s=="-help" || s=="-h" && isLast(1,argc))
+        {
+            help();
+        }
+
+        else if (s=="menu" || s=="-menu" && isLast(1,argc))
+        {
+            menu();
+        }
+
+        else if (s=="debug" || s=="-debug" && isLast(1,argc))
+        {
+            debug();
+        }
+
+        else if (s=="play" || s=="-play" || s=="-p")
+        {
+            game_data data;
+            for (int i = 2; i < argc; ++i)
+            {
+                string param(argv[i]);
+
+                if (param=="max" || param=="-max" || param=="-m")
+                {
+                    int max = getNextValue(i, argc, argv);
+                    if (max < 2)
+                    {
+                        cout << red << "Error!\t" << reset << "Invalid/undefined max value!"  << endl;
+                        return -1;
+                    }
+                    data.max = max;
+                }
+                else if(param=="length" || param=="-length" || param=="-l")
+                {
+                    int length = getNextValue(i, argc, argv);
+                    if (length < 1)
+                    {
+                        cout << red << "Error!\t" << reset << "Invalid/undefined length value!" << endl;
+                        return -1;
+                    }
+                    data.length = length;
+                }
+                else if(param=="size" || param=="-size" || param=="-s")
+                {
+                    int size = getNextValue(i, argc, argv);
+                    if (size < 1 || size > 3)
+                    {
+                        cout << red << "Error!\t" << reset << "Invalid/undefined size value!" << endl;
+                        return -1;
+                    }
+                    data.group_size = size;
+                }
+
+                else if(param=="single" || param=="-single" || param=="-s")
+                {
+                    data.group_size = 1;
+                }
+                else if(param=="PA" || param=="-PA" || param=="pa" || param=="-pa")
+                {
+                    data.group_size = 2;
+                }
+                else if(param=="PAO" || param=="-PAO" || param=="pao" || param=="-pao")
+                {
+                    data.group_size = 3;
+                }
+
+                else
+                {
+                    cout << red << "Error!\t" << reset << "Unknown game argument: " << argv[i] <<endl;
+                    return -1;
+                }
+            }
+            GameBuilder(data).run();
+            clear();
+        }
+
+        else
+        {
+            cout << red << "Error!\t" << reset << "Unknown argument: " << argv[1] <<endl;
+            return -1;
+        }
     }
     else
     {
@@ -65,4 +146,34 @@ void menu()
                 exit(0);
         }
     }
+}
+
+///
+/// @brief This function is responsible for the help screen.
+void help()
+{
+
+}
+
+///
+/// @brief Gets the next element of an array.
+/// @param index Current element. The function increments it twice.
+/// @param argc Array size.
+/// @param argv Array.
+/// @return The next element from the array, or -1 if the last one is current.
+int getNextValue(int &index, const int &argc, char *argv[])
+{
+    using namespace std;
+    ++index;
+   if (index >= argc) return -1;
+
+    string s(argv[index]);
+
+    if (!is_number(s)) return -1;
+    int i;
+    stringstream stream;
+
+    stream << s;
+    stream >> i;
+    return i;
 }
