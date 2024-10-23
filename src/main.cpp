982c6830ebb615c48f77060678a9d12a67526470
comradecobweb <comradecobweb@gmail.com>
2024-10-23 16:03:44 +0200

    Fixed argument validation

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,17 @@ int main(int argc, char *argv[])
     {
         string s(argv[1]);
 
-        if (s=="help" || s=="-help" || s=="-h" && isLast(1,argc))
+        if ((s=="help" || s=="-help" || s=="-h") && isLast(1,argc))
         {
             help();
         }
 
-        else if (s=="menu" || s=="-menu" && isLast(1,argc))
+        else if ((s=="menu" || s=="-menu") && isLast(1,argc))
         {
             menu();
         }
 
-        else if (s=="debug" || s=="-debug" && isLast(1,argc))
+        else if ((s=="debug" || s=="-debug") && isLast(1,argc))
         {
             debug();
         }
@@ -32,51 +32,81 @@ int main(int argc, char *argv[])
 
                 if (param=="max" || param=="-max" || param=="-m")
                 {
+                    if (data.max != -1)
+                    {
+                        cout << red << "Error!\t" << reset << "Duplicate argument: max"  << endl << endl;
+                        return -1;
+                    }
                     int max = getNextValue(i, argc, argv);
                     if (max < 2)
                     {
-                        cout << red << "Error!\t" << reset << "Invalid/undefined max value!"  << endl;
+                        cout << red << "Error!\t" << reset << "Invalid/undefined max value!"  << endl << endl;
                         return -1;
                     }
                     data.max = max;
                 }
                 else if(param=="length" || param=="-length" || param=="-l")
                 {
+                    if (data.length != -1)
+                    {
+                        cout << red << "Error!\t" << reset << "Duplicate argument: length"  << endl << endl;
+                        return -1;
+                    }
                     int length = getNextValue(i, argc, argv);
                     if (length < 1)
                     {
-                        cout << red << "Error!\t" << reset << "Invalid/undefined length value!" << endl;
+                        cout << red << "Error!\t" << reset << "Invalid/undefined length value!" << endl << endl;
                         return -1;
                     }
                     data.length = length;
                 }
                 else if(param=="size" || param=="-size" || param=="-s")
                 {
+                    if (data.group_size != -1)
+                    {
+                        cout << red << "Error!\t" << reset << "Duplicate argument: size"  << endl << endl;
+                        return -1;
+                    }
                     int size = getNextValue(i, argc, argv);
                     if (size < 1 || size > 3)
                     {
-                        cout << red << "Error!\t" << reset << "Invalid/undefined size value!" << endl;
+                        cout << red << "Error!\t" << reset << "Invalid/undefined size value!" << endl << endl;
                         return -1;
                     }
                     data.group_size = size;
                 }
 
-                else if(param=="single" || param=="-single" || param=="-s")
+                else if(param=="single" || param=="-single")
                 {
+                    if (data.group_size != -1)
+                    {
+                        cout << red << "Error!\t" << reset << "Duplicate argument: size"  << endl << endl;
+                        return -1;
+                    }
                     data.group_size = 1;
                 }
                 else if(param=="PA" || param=="-PA" || param=="pa" || param=="-pa")
                 {
+                    if (data.group_size != -1)
+                    {
+                        cout << red << "Error!\t" << reset << "Duplicate argument: size"  << endl << endl;
+                        return -1;
+                    }
                     data.group_size = 2;
                 }
                 else if(param=="PAO" || param=="-PAO" || param=="pao" || param=="-pao")
                 {
+                    if (data.group_size != -1)
+                    {
+                        cout << red << "Error!\t" << reset << "Duplicate argument: size"  << endl << endl;
+                        return -1;
+                    }
                     data.group_size = 3;
                 }
 
                 else
                 {
-                    cout << red << "Error!\t" << reset << "Unknown game argument: " << argv[i] <<endl;
+                    cout << red << "Error!\t" << reset << "Unknown game argument: " << argv[i] << endl << endl;
                     return -1;
                 }
             }
@@ -86,7 +116,7 @@ int main(int argc, char *argv[])
 
         else
         {
-            cout << red << "Error!\t" << reset << "Unknown argument: " << argv[1] <<endl;
+            cout << red << "Error!\t" << reset << "Unknown argument: " << argv[argc - 1] << endl << endl;
             return -1;
         }
     }
@@ -170,8 +200,9 @@ void help()
 int getNextValue(int &index, const int &argc, char *argv[])
 {
     using namespace std;
+
+    if (isLast(index, argc)) return -1;
     ++index;
-   if (index >= argc) return -1;
 
     string s(argv[index]);
 
