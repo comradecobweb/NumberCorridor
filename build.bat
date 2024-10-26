5c190cc3109a9ab979ca5249787124b2894196aa
comradecobweb <comradecobweb@gmail.com>
2024-10-26 12:05:19 +0200

    Fixed build.bat

--- build.bat
+++ build.bat
@@ -3,7 +3,5 @@ cd ..
 cmake -S .\NumberCorridor -B .\NumberCorridor\build-windows
 cd .\NumberCorridor
 cmake --build .\build-windows --config Release
-cd ..
-move /Y .\NumberCorridor\Release\NumberCorridor.exe .\NumberCorridor
-cd .\NumberCorridor
-rmdir .\Release
+move /Y .\out\Release\NumberCorridor.exe .\out\NumberCorridor.exe
+rmdir .\out\Release
