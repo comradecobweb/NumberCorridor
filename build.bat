cb94aac08975652cc3411cc6e44f23f2043f0bc4
comradecobweb <comradecobweb@gmail.com>
2024-11-17 10:53:02 +0100

    Fixed cmake command in build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 mkdir build-windows
 cd ..
-cmake -S .\NumberCorridor -B .\NumberCorridor\build-windows
+cmake -S .\NumberCorridor -B .\NumberCorridor\build-windows -G "MinGW Makefiles"
 cd .\NumberCorridor
 cmake --build .\build-windows --config Release
 move /Y .\out\Release\NumberCorridor.exe .\out\NumberCorridor.exe
